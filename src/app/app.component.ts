cdfda7f49ebf36004756d09c07fe269af3a96183
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-09 17:34:17 -0800

    binding to custom elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  newServerContent = '';
-
+  //newServerContent = '';
+  serverElements = [{type: 'server', name: 'Testserver';content: 'Just a test!';}];
 }
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -16,19 +16,19 @@ newServerName = '';
   }
 
   onAddServer() {
-  this.serverElements.push({
+  /*this.serverElements.push({
   type: 'server',
   name: this.newServerName,
   content: this.newServerContent
-  });
+  });*/
   }
 
 onAddBlueprint() {
-this.serverElements.push({
+/*this.serverElements.push({
 type: 'blueprint',
 name: this.newServerName,
 content: this.newServerContent
-});
+});*/
 }
 
 }
